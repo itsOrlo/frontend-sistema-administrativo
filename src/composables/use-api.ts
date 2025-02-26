5f9db7b6b6b075a2c441107666a72f955717b3dc
itsOrlo <orlo.montesdeoca@gmail.com>
2025-02-26 12:38:20 -0500

    Manejo de sesiones corregido

--- src/composables/use-api.ts
+++ src/composables/use-api.ts
@@ -1,5 +1,6 @@
 // useApi.js
-import axios from "axios";
+import axios from 'axios';
+import { useAutenticacionStore } from '@/stores/use-autenticacion.store';
 
 const useApi = axios.create({
   baseURL: "http://192.168.0.47:4000",
@@ -13,5 +14,16 @@ useApi.interceptors.request.use((config) => {
   }
   return config;
 });
+
+useApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const autenticacionStore = useAutenticacionStore();
+    if (error.response && error.response.status === 401) {
+      autenticacionStore.onLogout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export { useApi };
